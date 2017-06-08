6475750c8c4f30d94a4d2c080e61b2e2ac113360
Isabel <12ivazquez@gmail.com>
2017-06-08 23:06:13 +0000

    [#2/Isabel] has basic functionality w/ user stories

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const app = express()
+const app = express();
+const multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var port = process.env.PORT;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.sendFile(process.cwd() + '/index.html');
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.post('/get-file-size', upload.single('file'), function (req, res, next) {
+  res.send(JSON.stringify({"size":req.file.size}));
+});
+
+app.listen(port);
+console.log("Server is listening on port " + port);
